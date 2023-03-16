cf419545ea430a6b564aeab63e75d19633805739
Alejandro Jimenez <ajimenezal01@gmail.com>
2023-03-16 13:25:13 +0100

    Actualizacion Ej5

--- src/ej5/Estudiantes.java
+++ src/ej5/Estudiantes.java
@@ -7,10 +7,30 @@ public class Estudiantes {
     private int edad = 0;
     private double nota = 0.0;
 
-    public Estudiantes( String nombre,String sexo,int edad){
+    public Estudiantes( String nombre,String sexo,int edad,double nota){
         this.nombre=nombre;
         this.edad=edad;
         this.sexo=sexo;
         this.nota = nota;
     }
+ public void setEdad(int edad) {
+     this.edad = edad;
+ }
+ public void setNombre(String nombre) {
+     this.nombre = nombre;
+ }
+ public void setNota(double nota) {
+     this.nota = nota;
+ }
+ public void setSexo(String sexo) {
+     this.sexo = sexo;
+ }
+ 
+ public boolean hacerNovillos(){
+
+
+return false;
+
+ }
+
 }
--- src/ej5/Profesor.java
+++ src/ej5/Profesor.java
@@ -6,10 +6,24 @@ public class Profesor {
     private int edad = 0;
     private String materia = "";
 
-public Profesor( String nombre,String sexo,int edad){
+public Profesor( String nombre,String sexo,int edad, String materia){
     this.nombre=nombre;
     this.edad=edad;
     this.sexo=sexo;
     this.materia = materia;
 }
+
+public void setEdad(int edad) {
+    this.edad = edad;
+}
+public void setMateria(String materia) {
+    this.materia = materia;
+}
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+public void setSexo(String sexo) {
+    this.sexo = sexo;
+}
+
 }
